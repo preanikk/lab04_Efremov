706df595c333889b5a74add00b0658279320b68c
Ярослав <preanik1x@gmail.com>
2026-09-18 22:45:25 +0300

    Lab04: Logical Operators, Practice

--- Program.cs
+++ Program.cs
@@ -5,10 +5,43 @@
 // Console.WriteLine("Программа продолжает работу");
 
 
-int age = 15;
-if (age >= 18) {
-    Console.WriteLine("Доступ разрешен");
-} else {
-    Console.WriteLine("Доступ запрещен");
-    Console.WriteLine($"До совершеннолетия осталось: {18 - age} года");
+// int age = 15;
+// if (age >= 18) {
+//     Console.WriteLine("Доступ разрешен");
+// } else {
+//     Console.WriteLine("Доступ запрещен");
+//     Console.WriteLine($"До совершеннолетия осталось: {18 - age} года");
+// }
+
+
+// int age = 67;
+// if (age < 13)
+// {
+//     Console.WriteLine("Ребенок");
+// } else if (age < 18)
+// {
+//     Console.WriteLine("Подросток");
+// } else if (age < 60)
+// {
+//     Console.WriteLine("Взрослый");
+// } else
+// {
+//     Console.WriteLine("Пенсионер");
+// }
+
+
+int age = 16;
+double height = 1.4;
+bool soprovod = true;
+if (age >= 14 && height >= 1.5)
+{
+    Console.WriteLine("Можно кататься");
+} else if (age >= 14 && height < 1.5 && soprovod)
+{
+    Console.WriteLine("Можно кататься");
+} else
+{
+    Console.WriteLine("Пока нельзя");
 }
+
+
